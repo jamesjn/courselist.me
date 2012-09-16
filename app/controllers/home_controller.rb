b29b4fe91fcbb9c383955d19c8ac6d7bbfd4a523
jamesjn <james.jn.chiang@gmail.com>
2012-09-15 20:03:29 -0400

    fix bootstrap stylesheets again

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,16 @@
 class HomeController < ApplicationController
+  helper_method :start_date, :start_date
   def index
     @courses = Course.all
+  end
+
+  private
+
+  def start_date date
+    if date.blank?
+     "TBD" 
+    else
+      date
+    end
   end
 end
